e9e58bd876929a2ff68e4b16163ae17bf58c1865
Mihai Varga <me.shuv@gmail.com>
2021-06-03 20:57:48 +0300

    fixes port

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ mongoose.connect(dbConfig.url, {
 });
 
 require('./src/routes/routes.js')(app);
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
